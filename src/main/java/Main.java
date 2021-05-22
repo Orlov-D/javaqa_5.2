181045c5cd48268c0ab96450c44c6c46b5c67c6e
unknown <orlovdimas92@mail.ru>
2021-05-22 23:48:40 +0300

    fixes 2 Дополнен мэйн выводом результата.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
 
-    service.calculate(amount, registered);
-  }
+        long actual = service.calculate(amount, registered);
+// В принципе нормуль. Старой ошибки нету))) Можно писать, что исправлено)
+        long expected = 30;
+        boolean passed = expected == actual;
+
+        // выводим результат
+        if (passed) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+//        А что за ворнинги?
+//        WARNING: An illegal reflective access operation has occurred
+//        WARNING: Illegal reflective access by org.codehaus.groovy.reflection.ReflectionUtils (file:/C:/Users/Dima/.m2/repository/org/codehaus/groovy/groovy/3.0.0-beta-2/groovy-3.0.0-beta-2-indy.jar) to method java.util.Collections$UnmodifiableCollection.toString()
+//        WARNING: Please consider reporting this to the maintainers of org.codehaus.groovy.reflection.ReflectionUtils
+//        WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
+//        WARNING: All illegal access operations will be denied in a future release
+    }
 }
 
